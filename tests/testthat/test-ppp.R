c8e5d83e8f5cd3e47b1f7e26d1ddd62844468ef1
Laura Elgaard Iisager Jensen <au591556@uni.au.dk>
2024-08-22 15:35:40 +0200

    commit test4 to git

--- tests/testthat/test-ppp.R
+++ tests/testthat/test-ppp.R
@@ -1,3 +1,8 @@
-test_that("ppp_boxplot works with other data", {
-  for (vars in list(c("Sepal.Length", "Species"),
+test_that("ppp_boxplot works with another data set", {
+  plot <- ppp_boxplot(data = iris, 
+                       x_var = "Species", 
+                       y_var = "Sepal.Length", 
+                       comparisons = list(c("setosa", "versicolor"), c("setosa", "virginica")),
+                       colors = c("#337495", "#002a33", "#2f5a69"),
+                       method = "wilcox.test")
 })
